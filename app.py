7f91e45fc40c184f3b07ec73a011261d70be8737
Ahmad Rosid K <ahmadrosid30121997@gmail.com>
2020-04-29 14:20:30 +0700

    added flask to the project

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask
+from flask import render_template
+from flask import request
+from flask import send_from_directory
+from scraper import *
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+
+@app.route('/results')
+def results():
+    scraper = ProxyScraper()
+    scraper.run()
+
+    return render_template('results.html', proxies=scraper.results)
+
+
+@app.route('/download')
+def download():
+    return send_from_directory('', 'proxy_list.csv', as_attachment=True)
+
+
+if __name__ == '__main__':
+    app.run(debug=True, threaded=True)
