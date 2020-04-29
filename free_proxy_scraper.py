129903dfc3aeb165097588b149efa7c306ef0b37
Ahmad Rosid K <ahmadrosid30121997@gmail.com>
2020-04-29 14:07:25 +0700

    reformat scraper to use class

--- free_proxy_scraper.py
+++ free_proxy_scraper.py
@@ -3,32 +3,43 @@ from bs4 import BeautifulSoup
 import json
 import csv
 
-url = "https://www.free-proxy-list.net/"
 
-res = requests.get(url)
+class ProxyScraper:
+    results = []
 
-# print(res.status_code)
-# print(res.headers)
-# print(res.text)
+    def fetch(self, url):
+        """Fetch data from url"""
+        return requests.get(url)
+
+    def parse(self, html):
+        """Parse the result of fetch"""
+        soup = BeautifulSoup(html, 'html.parser')
+
+        table = soup.find('table')
+        rows = table.find_all('tr')
+
+        headers = [header.text for header in rows[0]]
+
+        self.results.append(headers)
+
+        for row in rows:
+            if len(row.find_all('td')) > 0:
+                data = [td.text for td in row]
+                self.results.append(data)
+
+    def to_csv(self):
+        """Export parsed data to csv"""
+        with open('proxy_list.csv', 'w') as output:
+            writer = csv.writer(output)
+            writer.writerows(self.results)
+
+    def run(self):
+        """Run the scraper"""
+        res = self.fetch('https://www.free-proxy-list.net/')
+        self.parse(res.text)
+        self.to_csv()
 
 
-results = []
-
-soup = BeautifulSoup(res.text, 'html.parser')
-table = soup.find('table')
-rows = table.find_all('tr')
-
-headers = [header.text for header in rows[0]]
-
-results.append(headers)
-
-for row in rows:
-    if len(row.find_all('td')) > 0:
-        data = [td.text for td in row]
-        results.append(data)
-
-print(json.dumps(results, indent=3))
-
-with open('proxy_list.csv', 'w') as output:
-    writer = csv.writer(output)
-    writer.writerows(results)
+if __name__ == '__main__':
+    scraper = ProxyScraper()
+    scraper.run()
